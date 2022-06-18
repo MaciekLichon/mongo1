bd7b55d50d5d13f96b142950143f4892f3f1449c
Maciej Lichoń <maciek.lichon@gmail.com>
2022-06-18 16:45:10 +0200

    Fix endpoint tests

--- test/api/departments/delete.test.js
+++ test/api/departments/delete.test.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 const request = chai.request;
 
-describe('GET /api/departments', () => {
+describe('DELETE /api/departments', () => {
 
   before(async () => {
     const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
